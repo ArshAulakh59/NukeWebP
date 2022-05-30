dbc6e4f8e6777e14d7ea4da7de1a4c6e10fb5045
ArshAulakh <arshaulakhios@gmail.com>
2022-05-30 18:28:46 +0530

    Updated minimum version for iOS.

--- Package.swift
+++ Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     name: "NukeWebP",
     platforms: [
         .macOS(.v10_13),
-        .iOS(.v11),
+        .iOS(.v13),
         .tvOS(.v11),
         .watchOS(.v4)
     ],
